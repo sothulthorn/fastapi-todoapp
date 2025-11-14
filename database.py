aa18a8fcd494b0c1dc4aabd068edd7ef321192c7
sothulthorn <thornsothul@gmail.com>
2025-11-14 22:26:49 +0900

    Change sqlite to postgresql

--- database.py
+++ database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///./todosapp.db'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:test1234!@localhost/TodoApplicationDatabase'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URI, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
